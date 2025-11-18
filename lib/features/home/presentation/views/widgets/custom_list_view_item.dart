aae8ddd959bbffd1572e315fd4dc1960991d51ce
HushABF <nkbh12342@gmail.com>
2025-11-18 11:05:13 +0200

    added custom list view item

---
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,27 @@
+
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: Container(
+          // height: MediaQuery.of(context).size.height * 0.2,
+          // width: MediaQuery.of(context).size.width * 0.3,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(AssetsData.testImage),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
