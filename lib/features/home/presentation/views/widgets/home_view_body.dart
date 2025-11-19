65f56f8a00b28d8b7ebe0d6127a70be49f5ae985
HushABF <nkbh12342@gmail.com>
2025-11-19 14:20:55 +0200

    best seller list view item done

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,10 +1,12 @@
 // import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 // import 'package:bookly_app/features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 // import 'package:booklylib/core/utils';
 
 class HomeViewBody extends StatelessWidget {
@@ -19,7 +21,7 @@ class HomeViewBody extends StatelessWidget {
         children: [
           CustomAppBar(),
           FeaturedListView(),
-          SizedBox(height: 80),
+          SizedBox(height: 50),
           Text('Best Seller', style: Styles.textStyle18),
           SizedBox(height: 30),
           BestSellerListViewItem(),
@@ -29,33 +31,3 @@ class HomeViewBody extends StatelessWidget {
   }
 }
 
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 130,
-
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2 / 3,
-            child: Container(
-              margin: const EdgeInsets.only(right: 15),
-              // height: MediaQuery.of(context).size.height * 0.2,
-              // width: MediaQuery.of(context).size.width * 0.3,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(AssetsData.testImage),
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ),
-          ),
-          const Column(children: []),
-        ],
-      ),
-    );
-  }
-}
