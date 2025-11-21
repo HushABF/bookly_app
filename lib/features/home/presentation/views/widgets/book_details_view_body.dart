cc1d9dc5730993768fb19baeabdb88291c9eae53
HushABF <nkbh12342@gmail.com>
2025-11-21 09:40:33 +0200

    added book rating to book details view and did some changes to it

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -11,14 +13,28 @@ class BookDetailsViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
+        // crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           const CustomBooksDetailsAppBar(),
           Padding(
             padding: EdgeInsets.symmetric(
-              horizontal: width * 0.17,
+              horizontal: width * 0.22,
               vertical: 36,
             ),
             child: const CustomBookImage(),
+          ),
+          const Text('The Jungle Book', style: Styles.textStyle30),
+          const SizedBox(height: 6),
+          Opacity(
+            opacity: 0.7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18.copyWith(fontStyle: FontStyle.italic),
+            ),
+          ),
+          const SizedBox(height: 12),
+          const Center(
+            child: BookRating(mainAxisAlignment: MainAxisAlignment.center),
           ),
         ],
       ),
